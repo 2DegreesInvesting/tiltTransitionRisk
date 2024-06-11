061f31abc7c0a053da6a806c880feb821fa2c2ec
Kalash Singhal <125359076+kalashsinghal@users.noreply.github.com>
2024-06-11 15:33:11 +0200

    rename arguments (#19)

--- R/add_transition_risk_category.R
+++ R/add_transition_risk_category.R
@@ -13,13 +13,13 @@
 #' library(dplyr)
 #' options(readr.show_col_types = FALSE)
 #'
-#' emissions_profile_products <- read_csv(toy_emissions_profile_products_ecoinvent())
-#' all_uuids_scenario_sectors <- read_csv(toy_all_uuids_scenario_sectors())
+#' co2 <- read_csv(toy_emissions_profile_products_ecoinvent())
+#' all_activities_scenario_sectors <- read_csv(toy_all_uuids_scenario_sectors())
 #' scenarios <- read_csv(toy_sector_profile_any_scenarios())
 #'
 #' transition_risk_thresholds <- add_thresholds_transition_risk(
-#'   emissions_profile_products,
-#'   all_uuids_scenario_sectors,
+#'   co2,
+#'   all_activities_scenario_sectors,
 #'   scenarios
 #' )
 #'
